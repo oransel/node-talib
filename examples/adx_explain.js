e15b9c0b6bdf70a7d747ecbd688183146de898e8
Mustafa Oransel <oransel@yahoo.com>
2012-01-26 14:20:42 -0800

    explain example

---
+++ examples/adx_explain.js
@@ -0,0 +1,9 @@
+var talib = require("../build/Release/talib");
+var functions = talib.functions;
+
+console.log();
+console.log("TALib Version: " + talib.version);
+
+console.log(talib.explain("ADX"));
+
+
