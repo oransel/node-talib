02053e14bbfbac0f5e8fb4aa88532de73e752dec
Mustafa Oransel <oransel@yahoo.com>
2012-02-27 22:52:08 -0800

    osx 64bit fix

--- src/lib/build.js
+++ src/lib/build.js
@@ -1,4 +1,4 @@
 var exec = require('child_process').exec;
-var arch = process.arch == 'ia32' ? 'i386' : process.arch;
+var arch = process.arch == 'ia32' ? 'i386' : process.arch == 'x64' ? 'x86_64' : process.arch;
 console.log('building talib functions...');
 exec('export CFLAGS="-arch ' + arch + '" && export LDFLAGS="-arch ' + arch + '" && ./configure && make ARCH=' + arch);
