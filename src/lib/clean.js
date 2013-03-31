fe5e9eecde0505be4fe9168ed335517995d929cb
Mustafa Oransel <oransel@yahoo.com>
2013-03-31 14:42:58 -0700

    fixed build script for talib and added clean script

--- src/lib/clean.js
+++ src/lib/clean.js
@@ -1,10 +1,10 @@
 var exec = require('child_process').exec;
 
-console.log('building talib functions...');
+console.log('cleaning talib...');
 var flags = "";
 if (process.platform == 'darwin') {
 	var arch = process.arch == 'ia32' ? 'i386' : process.arch == 'x64' ? 'x86_64' : process.arch;
 	flags = 'export CFLAGS="-arch ' + arch + '" && export LDFLAGS="-arch ' + arch + '" && ';
 }
 process.chdir('make/csr/linux/g++/');
-exec(flags + 'make');
+exec(flags + 'make clean');
