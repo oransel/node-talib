220268f3f3efc660c71d3b8ae76508714ddeaf7a
Alfred Omega <alfred.omega@gmail.com>
2013-05-08 20:15:03 -0700

    Fixed the inReal parameter passing

---
+++ examples/sma.js
@@ -0,0 +1,25 @@
+var talib = require("../build/Release/talib");
+var fs = require("fs");
+
+// Display module version
+console.log();
+console.log("TALib Version: " + talib.version);
+
+// Load market data
+var marketContents = fs.readFileSync('examples/marketdata.json','utf8'); 
+var marketData = JSON.parse(marketContents);
+
+// execute ADX indicator function with time period 9
+talib.execute({
+    name: "SMA",
+    startIdx: 0,
+    endIdx: marketData.close.length - 1,
+    inReal: marketData.close,
+    optInTimePeriod: 180
+}, function (result) {
+	
+	// Show the result array
+    console.log("SMA Function Results:");
+    console.log(result);
+
+});
