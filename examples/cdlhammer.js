8bf15f731327c3ad1b48b0e3e5fe252a61e6a9b2
Mustafa Oransel <oransel@yahoo.com>
2014-02-14 08:38:51 -0800

    added CDLHAMMER example

---
+++ examples/cdlhammer.js
@@ -0,0 +1,27 @@
+var talib = require("../build/Release/talib");
+var fs = require("fs");
+
+// Display module version
+console.log();
+console.log("TALib Version: " + talib.version);
+
+// Load market data
+var marketContents = fs.readFileSync('examples/test.json','utf8'); 
+var marketData = JSON.parse(marketContents);
+
+// execute CDLHAMMER indicator function
+talib.execute({
+    name: "CDLHAMMER",
+    startIdx: 0,
+    endIdx: marketData.close.length - 1,
+    open: marketData.open,
+    high: marketData.high,
+    low: marketData.low,
+    close: marketData.close
+}, function (result) {
+	
+	// Show the result array
+    console.log("CDLHAMMER Function Results:");
+    console.log(result);
+
+});
