cf21e39b500a0a39166f5c047bbd55289eb2549a
Mustafa Oransel <oransel@Mustafa-MBPro.local>
2018-01-11 21:35:58 -0800

    msbuild fixes for ta_abstract and ta_libc for csr|Win32 targets

--- binding.gyp
+++ binding.gyp
@@ -1,45 +1,43 @@
 {
-    'targets': [
-        {
-            'target_name': 'talib',
-            'sources': [
-                'src/talib.cpp'
-            ],
-            "include_dirs" : [
-                "<!(node -e \"require('nan')\")"
-            ],
-            'conditions': [
-              ['OS=="linux"', {
-              "libraries" : [
-                "../src/lib/lib/libta_abstract_csr.a",
-                "../src/lib/lib/libta_func_csr.a",
-                "../src/lib/lib/libta_common_csr.a",
-                "../src/lib/lib/libta_libc_csr.a",
-              ]
+    'targets': [{
+        'target_name': 'talib',
+        'sources': [
+            'src/talib.cpp'
+        ],
+        "include_dirs": [
+            "<!(node -e \"require('nan')\")"
+        ],
+        'conditions': [
+            ['OS=="linux"', {
+                "libraries": [
+                    "../src/lib/lib/libta_abstract_csr.a",
+                    "../src/lib/lib/libta_func_csr.a",
+                    "../src/lib/lib/libta_common_csr.a",
+                    "../src/lib/lib/libta_libc_csr.a",
+                ]
             }],
-              [ 'OS=="mac"', {
-                  'xcode_settings': {
-                    'OTHER_CPLUSPLUSFLAGS' : ['-std=c++11', '-stdlib=libc++'],
+            ['OS=="mac"', {
+                'xcode_settings': {
+                    'OTHER_CPLUSPLUSFLAGS': ['-std=c++11', '-stdlib=libc++'],
                     'OTHER_LDFLAGS': ['-stdlib=libc++'],
                     'MACOSX_DEPLOYMENT_TARGET': '10.7',
                     'GCC_ENABLE_CPP_EXCEPTIONS': 'YES'
-                    },
-                    "libraries" : [
-                      "../src/lib/lib/libta_abstract_csr.a",
-                      "../src/lib/lib/libta_func_csr.a",
-                      "../src/lib/lib/libta_common_csr.a",
-                      "../src/lib/lib/libta_libc_csr.a",
-                    ]
-                }],
-              [ 'OS=="win"', {
-                "libraries" : [
-                   "../src/lib/lib/ta_libc_csr.lib",
-                   "../src/lib/lib/ta_func_csr.lib",
-                   "../src/lib/lib/ta_common_csr.lib",
-                   "../src/lib/lib/ta_abstract_csr.lib"
+                },
+                "libraries": [
+                    "../src/lib/lib/libta_abstract_csr.a",
+                    "../src/lib/lib/libta_func_csr.a",
+                    "../src/lib/lib/libta_common_csr.a",
+                    "../src/lib/lib/libta_libc_csr.a",
                 ]
-              }],
-            ]
-        }
-    ]
+            }],
+            ['OS=="win"', {
+                "libraries": [
+                    "../src/lib/lib/ta_libc_csr.lib",
+                    "../src/lib/lib/ta_func_csr.lib",
+                    "../src/lib/lib/ta_common_csr.lib",
+                    "../src/lib/lib/ta_abstract_csr.lib"
+                ]
+            }],
+        ]
+    }]
 }
--- src/lib/build.js
+++ src/lib/build.js
@@ -4,50 +4,49 @@ var fs = require('fs');
 console.log('building talib functions...');
 
 if (process.platform == 'win32') {
-  var msbuildPath = '"C:/Program Files';
-  var frameworkPathStart = 'C:/Program Files';
-  var frameworkPathEnd = '/Reference Assemblies/Microsoft/Framework/.NETFramework';
-  var platform = 'Win32'
-  if(process.arch == 'x64') {
-    msbuildPath += ' (x86)'
-    platform = 'x64'
+
+  let msbuildPath;
+  let frameworkPath;
+  let arch;
+
+  if (process.arch == 'x64') {
+    msbuildPath = '"C:/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe"';
+    frameworkPath = 'C:/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework';
+    arch = 'x64';
+  } else {
+    msbuildPath = '"C:/Program Files/MSBuild/14.0/Bin/MSBuild.exe"';
+    frameworkPath = 'C:/Program Files/Reference Assemblies/Microsoft/Framework/.NETFramework';
+    arch = 'Win32';
   }
 
-  msbuildPath += '/MSBuild/14.0/Bin/';
+  var frameworkVersions = fs.readdirSync(frameworkPath).map(function(val, i){
+    return { index: val, path: frameworkPath + '/' + val };
+  });
 
-  var stats32, stats64, frameworkVersions = [], frameworkVersion;
-  try {
-    var stats32 = fs.statSync(frameworkPath + frameworkPathEnd);
-    frameworkVersions.concat(fs.readdirSync(frameworkPath + frameworkPathEnd).map(function(val, i){
-      return { index: val, path: frameworkPathStart + frameworkPathEnd + '/' + val };
-    }));
-  } catch (error) {}
-  try {
-    var stats64 = fs.statSync(frameworkPathStart + ' (x86)' + frameworkPathEnd);
-    frameworkVersions = frameworkVersions.concat(fs.readdirSync(frameworkPathStart + ' (x86)' + frameworkPathEnd).map(function(val, i){
-      return { index: val, path: frameworkPathStart + ' (x86)' + frameworkPathEnd + '/' + val };
-    }));
-  } catch (error) {}
-  
   if(!frameworkVersions.length){
-    console.log('No .NETFramework versions installed!')
+    return console.error('No .NETFramework versions installed!');
   } else {
     frameworkVersions.sort(function(a,b){
-      return (a.index > b.index) ? 1 : ((a.index < b.index) ? -1 : 0)
+      return (a.index > b.index) ? 1 : ((a.index < b.index) ? -1 : 0);
     });
     frameworkVersion = frameworkVersions.pop().path;
   }
 
   process.chdir('./src/lib/make/csr/windows/msbuild/');
-  exec(msbuildPath + 'MSBuild.exe" ./ta_lib.sln /p:FrameworkPathOverride="'+frameworkVersion+'" /property:Configuration=csr /property:Platform=' + platform, function(err, stdout, stderr) {
+  exec(msbuildPath + ' ./ta_lib.sln /p:FrameworkPathOverride="' + frameworkVersion + '" /property:Configuration=csr /property:Platform=' + arch, function(err, stdout, stderr) {
     console.log(stdout, stderr);
   });
+
 } else {
+
   var flags = '';
   if (process.platform == 'darwin') {
     var arch = process.arch == 'ia32' ? 'i386' : process.arch == 'x64' ? 'x86_64' : process.arch;
     flags = 'MACOSX_DEPLOYMENT_TARGET=10.7 export CFLAGS="-arch ' + arch + '" && export LDFLAGS="-arch ' + arch + '" && ';
   }
   process.chdir('./src/lib/make/csr/linux/g++/');
-  exec(flags + 'make');
+  exec(flags + 'make', function(err, stdout, stderr) {
+    console.log(stdout, stderr);
+  });
+
 }
